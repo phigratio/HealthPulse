d8febd8636264e386e94b6244497ccf19e034803
phigratio <muqtadir.mujahid02@gmail.com>
2024-06-26 15:20:36 +0600

    First Commit and initialized the project

---
+++ healthpulse-app-apis/src/main/java/com/healthpulse/website/HealthpulseAppApisApplication.java
@@ -0,0 +1,13 @@
+package com.healthpulse.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthpulseAppApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthpulseAppApisApplication.class, args);
+	}
+
+}
