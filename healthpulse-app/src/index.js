b41482bb8d3574df6a257c4aa3b37feb1eef5efa
phigratio <muqtadir.mujahid02@gmail.com>
2024-06-28 15:04:08 +0600

    working with my projects reactstrap

--- healthpulse-app/src/index.js
+++ healthpulse-app/src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import About from "./components/About";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <About />
+    <App />
   </React.StrictMode>
 );
 
